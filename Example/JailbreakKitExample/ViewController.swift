22cc3326489ee2e3cbfa45eae7dd5313d8039669
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2019-04-04 18:49:36 +0100

    initial project setup

---
+++ Example/JailbreakKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JailbreakKitExample
+//
+//  Created by Stoyan Stoyanov on 04/04/2019.
+//  Copyright © 2019 Stoyan Stoyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Source/JailBreakTest/HN4ZHNjZHNhY.h
@@ -0,0 +1,19 @@
+//
+//  HN4ZHNjZHNhY.h
+//  HN4ZHNjZHNhY
+//
+//  Created by Stoyan Stoyanov on 04/04/2019.
+//  Copyright © 2019 Stoyan Stoyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HN4ZHNjZHNhY.
+FOUNDATION_EXPORT double HN4ZHNjZHNhYVersionNumber;
+
+//! Project version string for HN4ZHNjZHNhY.
+FOUNDATION_EXPORT const unsigned char HN4ZHNjZHNhYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HN4ZHNjZHNhY/PublicHeader.h>
+
+
